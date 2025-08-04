f98707d34476eec1078a50e7d51f999cd6a92ee7
whackhack24 <vanshikabansal9319@gmail.com>
2025-08-04 16:44:52 +0530

    snake movement and score updation

--- index.js
+++ index.js
@@ -3,39 +3,60 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const reset=document.getElementById('reset-btn');
     const tile=document.getElementsByClassName('block');
     const gameOver=document.querySelector('.game-over');
+    const score=document.getElementById('score');
 
     let snakeMouth;
     let snakeMouthIndex;
     let intervalId;
+    let snakeLength=1;
+    let foodIndex;
     function generateFood() {
         //196
-        let index=Math.floor(Math.random()*196);
-        let foodCell=tile[index];
+        foodIndex=Math.floor(Math.random()*196);
+        let foodCell=tile[foodIndex];
         foodCell.style.backgroundColor='red';
     }
 
-    function snakeDirection(e){
-        if(e.key==="ArrowUp")
-            snakeMouth=tile[snakeMouthIndex-14];
-        else if(e.key==="ArrowDown")
-            snakeMouth=tile[snakeMouthIndex+14];
-        else if(e.key==="ArrowLeft")
-            snakeMouth=tile[snakeMouthIndex-1];
-        else if(e.key==="ArrowRight")
-            snakeMouthIndex=tile[snakeMouthIndex+1];
-    }
+    let currentDirection;
     function snake() {
         document.addEventListener('keydown', (e)=>{
-            snakeDirection(e);
+            currentDirection=e.key;
         });
-        
+        snakeMouth.style.backgroundColor='rgb(255,255,158)';
+        if(currentDirection==="ArrowUp")
+        {
+            snakeMouth=tile[snakeMouthIndex-14];
+            snakeMouthIndex-=14;
+        }
+        else if(currentDirection==="ArrowDown")
+        {
+            snakeMouth=tile[snakeMouthIndex+14];
+            snakeMouthIndex+=14;
+        }
+        else if(currentDirection==="ArrowLeft")
+        {
+            snakeMouth=tile[snakeMouthIndex-1];
+            snakeMouthIndex-=1;
+        }
+        else if(currentDirection==="ArrowRight")
+        {
+            snakeMouth=tile[snakeMouthIndex+1];
+            snakeMouthIndex+=1;
+        }
+        //updating score
+        if(snakeMouthIndex===foodIndex)
+        {
+            snakeLength+=1;
+            score.textContent=parseInt(score.textContent)+1;
+            generateFood();
+        }
+        snakeMouth.style.backgroundColor='green';
     }
     start.addEventListener('click', ()=>{
         start.disabled=true;
         snakeMouth=tile[104];
         snakeMouthIndex=104;
         snakeMouth.style.backgroundColor='green';
-        //updating score
         //generating food
         generateFood();
         //snake(moving, length increment, collision)
